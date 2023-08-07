f86429f46edd0e7862d0b81610ee4e614bf77c93
Josh W <wilson.jd022@gmail.com>
2023-08-07 16:41:22 -0700

    login success

--- src/components/loginBox/component.tsx
+++ src/components/loginBox/component.tsx
@@ -1,13 +1,13 @@
 import { FunctionComponent } from "react";
 import { Box, Flex, Label, Text, TextField, Button } from "gestalt";
 
-const enum LoginChangeOption {
+export const enum LoginChangeOption {
   UserName,
   Password,
 }
 
 interface Props {
-  loginTrigger: () => void;
+  loginTrigger: () => Promise<void>;
   userName: string;
   password: string;
   onChange: (option: LoginChangeOption, value: string) => void;
@@ -20,43 +20,49 @@ export const LoginBoxComponent: FunctionComponent<Props> = ({
   onChange,
 }) => {
   return (
-    <Flex justifyContent="center" height="100" width="100%">
-      <form onSubmit={(e) => e.preventDefault()}>
-        <Flex direction="column" gap={2}>
-          <Label htmlFor="username">
-            <Text>Username</Text>
-          </Label>
+    <Box padding={4}>
+      <Flex justifyContent="center" width="100%" maxWidth={800}>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <Flex direction="column" gap={2}>
+            <Label htmlFor="username">
+              <Text>Username</Text>
+            </Label>
 
-          <TextField
-            id="username"
-            onChange={({ value }) => {
-              onChange(LoginChangeOption.UserName, value);
-            }}
-            value={userName}
-            autoComplete="username"
-            type="text"
-            // placeholder=""
-          />
+            <TextField
+              id="username"
+              onChange={({ value }) => {
+                onChange(LoginChangeOption.UserName, value);
+              }}
+              value={userName}
+              autoComplete="username"
+              type="text"
+            />
 
-          <Label htmlFor="password">
-            <Text>Password</Text>
-          </Label>
+            <Label htmlFor="password">
+              <Text>Password</Text>
+            </Label>
 
-          <TextField
-            id="password"
-            onChange={({ value }) => {
-              onChange(LoginChangeOption.Password, value);
-            }}
-            value={password}
-            autoComplete="off"
-            type="password"
-            // placeholder=""
-          />
+            <TextField
+              id="password"
+              onChange={({ value }) => {
+                onChange(LoginChangeOption.Password, value);
+              }}
+              value={password}
+              autoComplete="off"
+              type="password"
+            />
 
-          <Button text="Login" size="lg" color="blue" />
-          <Button size="sm" text="Don't have an Account?" />
-        </Flex>
-      </form>
-    </Flex>
+            <Button
+              text="Login"
+              size="md"
+              fullWidth={true}
+              color="blue"
+              onClick={() => loginTrigger()}
+            />
+            <Button size="md" text="Create Account" fullWidth={true} />
+          </Flex>
+        </form>
+      </Flex>
+    </Box>
   );
 };
--- src/components/loginBox/container.tsx
+++ src/components/loginBox/container.tsx
@@ -1,15 +1,34 @@
-import { FunctionComponent } from "react";
-import { LoginBoxComponent } from "./component";
+import { FunctionComponent, useState } from "react";
+import { LoginBoxComponent, LoginChangeOption } from "./component";
 import axios from "axios";
 
 export const LoginBox: FunctionComponent = () => {
-  const performLogin = async (name: string, password: string) => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (option: LoginChangeOption, value: string) => {
+    switch (option) {
+      case LoginChangeOption.UserName:
+        setUserName(value);
+        break;
+      case LoginChangeOption.Password:
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const performLogin = async () => {
     try {
       const response = await axios.post<boolean>(
         "http://localhost:4000/auth/login",
         {
-          name,
+          name: userName,
           password,
+        },
+        {
+          method: "POST",
         }
       );
 
@@ -22,5 +41,12 @@ export const LoginBox: FunctionComponent = () => {
       console.error(error);
     }
   };
-  return <LoginBoxComponent />;
+  return (
+    <LoginBoxComponent
+      userName={userName}
+      password={password}
+      loginTrigger={performLogin}
+      onChange={handleChange}
+    />
+  );
 };
